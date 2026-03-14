ff03160f38f98b4d978df83be5e03db68a84e505
Paolo Audiberti <pta200@gmail.com>
2026-03-14 18:55:18 -0400

    fix dockerfile and add docker compose with postgresql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
 	 
 	INSERT INTO city (cityid, city, state, province, country) VALUES
-	  (random_uuid() ,'New York', 'New York', '', 'US'),
-	  (random_uuid() ,'Chicago', 'Illinois','', 'US');
+	  ('12d8c877-3a91-4290-8d30-103beb105352' ,'New York', 'New York', '', 'US'),
+	  ('8df8811f-c183-4609-8e43-e75ce343a08d','Chicago', 'Illinois','', 'US');
